2f7c0a4be281824c4417bb9a22f9283435436325
Suyog Kulkarni <125109134+suyog-2023@users.noreply.github.com>
2023-03-06 21:40:39 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -4,9 +4,6 @@ const mongoose = require('mongoose');
 // creating a database
 mongoose.set('strictQuery', true);
 mongoose.connect("mongodb://localhost:27017/webapp", {
-    // useCreateIndex: true,
-    // useNewUrlParser: true,
-    // useUnifiedTopology:true
 }).then(() => {
     console.log("connection successful")
 }).catch((error) => {
